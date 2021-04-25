42fb7bf8baafecadd6acd9866cbe16637e78a8f2
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-04-25 13:47:49 -0500

    added react methods. added gallery image

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
 // gallery component
 function Gallery(props) {
     // new object that set its name and description to values for the Commercial page. 
     const currentCategory = {
         name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    };
+        description:
+          "Photos of grocery stores, food trucks, and other commercial projects",
+      };
     return (
         // // capitalizeFirstLetter() function to capitalize the name value when it's rendered
         <section>
         <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
         <p>{currentCategory.name}</p>
-        </section>
+        <div>
+            <img
+              src={photo} alt="Commercial Example" className="img-thumbnail mx-1"/>
+        </div>
+      </section>
     );
-}
+  }
+
 
 export default Gallery;
