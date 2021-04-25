50c526b36c2469a901a65c6812a3398720b083c0
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-04-24 23:32:40 -0500

    added gallery component

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+// gallery component
+function Gallery(props) {
+    // new object that set its name and description to values for the Commercial page. 
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects",
+    };
+    return (
+        // // capitalizeFirstLetter() function to capitalize the name value when it's rendered
+        <section>
+        <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+        <p>{currentCategory.name}</p>
+        </section>
+    );
+}
+
+export default Gallery;
