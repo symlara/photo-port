3e2e1696130e11a11809f7fb11b5f709b28e3b9f
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-04-26 18:30:51 -0500

    added conditional rendering and an SPA

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
+
 function App() {
+  const [contactSelected, setContactSelected] =  useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -20,14 +23,21 @@ function App() {
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+      {!contactSelected ? (
+  <>
+    <Gallery currentCategory={currentCategory}></Gallery>
+    <About></About>
+  </>
+) : (
+    <ContactForm></ContactForm>
+  )}
       </main>
     </div>
   );
